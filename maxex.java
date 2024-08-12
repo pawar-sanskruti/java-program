b3cac80b62cafb6661f6e44adf8ad7e03bac358e
pawar-sanskruti <158493589+pawar-sanskruti@users.noreply.github.com>
2024-08-12 17:58:44 +0530

    Add files via upload

---
+++ maxex.java
@@ -0,0 +1,13 @@
+
+
+public class maxex { 
+  
+    public static void main(String args[]) 
+    { 
+        int a = 25; 
+        int b = 23; 
+  
+        // prints the maximum of two numbers 
+        System.out.println(Math.max(a, b)); 
+    } 
+} 
