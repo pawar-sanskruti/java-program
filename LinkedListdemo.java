f4f85ddf1c3e8bc8efe287feaafcdcee1e28b715
pawar-sanskruti <158493589+pawar-sanskruti@users.noreply.github.com>
2025-01-05 23:34:51 +0530

    Add files via upload

---
+++ LinkedListdemo.java
@@ -0,0 +1,15 @@
+import java.util.LinkedList;
+
+public class LinkedListdemo {
+    public static void main(String[] args){
+        LinkedList list=new LinkedList();
+        list.add("sanskruti");
+        list.add(20);
+        list.add(12.9);
+        list.add("pawar");
+        list.add(null);
+        System.out.println(list);
+
+    }
+    
+}
